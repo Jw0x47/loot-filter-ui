6e6c8ab99600b60a428e3c1f52feba20d5d875b6
Kaqemeex <dev@kaqemeex.net>
2025-02-16 15:08:49 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Welcome to My GitHub Pages Site</h1>
+    <p>This site is deployed using GitHub Actions!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style> 
